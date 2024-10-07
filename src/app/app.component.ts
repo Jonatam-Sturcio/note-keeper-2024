8520fd0d2ca703249c96e0f4b317df712d658d0e
Jonatam Sturcio <bitcomanderbr@gmail.com>
2024-10-07 14:37:10 -0300

    Bibliotecas importadas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { ShellComponent } from './core/components/shell/shell.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [MatCardModule, ShellComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'note-keeper-2024';
-}
+export class AppComponent {}
