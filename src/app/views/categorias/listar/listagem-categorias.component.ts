820b4b8d6e87693f18c7ad9b31c5e541bffb032a
Jonatam Sturcio <bitcomanderbr@gmail.com>
2024-10-08 14:35:42 -0300

    Adicionada listage de categorias

---
+++ src/app/views/categorias/listar/listagem-categorias.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { ListagemCategoria } from '../models/categoria.models';
+import { NgForOf } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { CategoriaService } from '../services/categoria.service';
+
+@Component({
+  selector: 'app-listagem-categorias',
+  standalone: true,
+  imports: [
+    NgForOf,
+    RouterLink,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTooltipModule,
+  ],
+  templateUrl: './listagem-categorias.component.html',
+  styleUrl: './listagem-categorias.component.scss',
+})
+export class ListagemCategoriasComponent implements OnInit {
+  categorias: ListagemCategoria[];
+
+  constructor(private categoriaService: CategoriaService) {
+    this.categorias = [];
+  }
+  ngOnInit(): void {
+    this.categoriaService.selecionarTodos().subscribe((res) => {
+      this.categorias = res;
+    });
+  }
+}
--- src/app/views/categorias/services/categoria.service.ts
+++ src/app/views/categorias/services/categoria.service.ts
@@ -1,11 +1,18 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../../../environments/environment.development';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/internal/Observable';
+import { ListagemCategoria } from '../models/categoria.models';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CategoriaService {
   private readonly url = `${environment.API_URL}/categorias`;
+
   constructor(private http: HttpClient) {}
+
+  selecionarTodos(): Observable<ListagemCategoria[]> {
+    return this.http.get<ListagemCategoria[]>(this.url);
+  }
 }
